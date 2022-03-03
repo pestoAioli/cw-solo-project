dc9b617dbdf850ba178272dc8abdd092d590323b
Adrià M. Cassorla <adria@adriamartinez.cat>
2022-03-03 13:34:58 +0100

    Basic server finished

--- Server/src/GraphQL/resolvers/query.resolver.js
+++ Server/src/GraphQL/resolvers/query.resolver.js
@@ -1,21 +1,25 @@
 // @ts-nocheck
-"use strict";
-const fetch = require("node-fetch");
-const { getTomTomURL } = require("./../../Helpers/routes");
+'use strict';
+const fetch = require('node-fetch');
+const {
+  getTomTomURL,
+  getRandomDestination,
+} = require('./../../Helpers/routes');
 
 exports.getLocation = async (_, args) => {
-  const { start, range, filters, type } = args.input;
-
-  // TODO: call a helper function with a starting point, range and filters to get a destination (id + location);
-  const dest = { id: "1234", location: [41.423, 2.119] };
-
-  const url = getTomTomURL(start, dest.location, type);
+  const { origin, range, filters, type } = args.input;
 
   try {
+    const dest = await getRandomDestination(origin, range, filters);
+    if (!dest) throw e;
+
+    const url = getTomTomURL(origin, dest.coordinates, type);
     const result = await fetch(url);
     const route = await result.json();
+
     return { id: dest.id, route };
   } catch (e) {
-    throw e;
+    console.error(e);
+    return { id: 'error', route: [] };
   }
 };
--- Server/src/Helpers/routes.js
+++ Server/src/Helpers/routes.js
@@ -1,8 +1,9 @@
 // @ts-nocheck
 "use strict";
+const { Locations } = require("./../Models");
 
 // Creates a url to call the TomTom API
-exports.getTomTomURL = (start, dest, type) => {
+const getTomTomURL = (start, dest, type) => {
   const baseURL = "https://api.tomtom.com/routing/1/calculateRoute/";
   const coords = `${start[0]}%2C${start[1]}%3A${dest[0]}%2C${dest[1]}/json`;
 
@@ -24,3 +25,41 @@ exports.getTomTomURL = (start, dest, type) => {
 
   return url;
 };
+
+// Credit to ParkPal (Guillem, Nelson, Viktor, and Brandon)
+// Original source: https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates
+const filterByDistance = (origin, dest, range) => {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(dest[0] - origin[0]);
+  const dLon = deg2rad(dest[1] - origin[1]);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(origin[0])) *
+      Math.cos(deg2rad(dest[0])) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d < range;
+};
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+const getRandomDestination = async (origin, range, filters) => {
+  // TODO: Make the filter at the db, and just receive locations that match filters.
+  const allLocations = await Locations.find().exec();
+  if (allLocations.length === 0) throw new Error();
+
+  const filteredLocs = allLocations.filter(
+    (loc) =>
+      filterByDistance(origin, loc.coordinates, range) &&
+      loc.tags.includes(filters[0])
+  );
+
+  const rand = Math.floor(Math.random() * filteredLocs.length);
+  return filteredLocs[rand];
+};
+
+module.exports = { getTomTomURL, getRandomDestination };
