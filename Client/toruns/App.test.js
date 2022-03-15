93b2581f9af0594765792c897cc76244fb43e5b3
Ricky <drickyriverab@gmail.com>
2022-03-15 15:19:35 +0100

    two tests on front end

--- Client/toruns/App.test.js
+++ Client/toruns/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import renderer, { act } from "react-test-renderer";
+import renderer from "react-test-renderer";
 import App from "./App";
 import { cleanup } from '@testing-library/react-native';
 
@@ -10,7 +10,7 @@ afterEach(cleanup);
 
 describe('<App />', () => {
   it('has 1 child', async () => {
-    const tree = await renderer.create(<App />).toJSON();
+    const tree = await renderer.create(<App />).toJSON()
     expect(tree.children.length).toBe(1);
   })
 }) 
--- Client/toruns/Components/Buttons/tests/CircleButton.test.js
+++ Client/toruns/Components/Buttons/tests/CircleButton.test.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import renderer from "react-test-renderer";
-import { act } from 'react-dom/test-utils';
-
 import { cleanup } from '@testing-library/react-native';
+import CircleButton from '../CircleButton';
 
 beforeEach(() => {
   jest.useFakeTimers();
 })
 afterEach(cleanup);
 
-describe('<CircleButton/>', () => {
-  it('should render a button', async () => {
-
+describe('<CircleButton/>',() => {
+  it('should render a component', async () => {
+    const tree = await renderer.create(<CircleButton/>).toTree()
+    expect(tree.nodeType).toBe('component')
   })
 })
